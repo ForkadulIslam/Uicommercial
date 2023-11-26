e47f0ce3ac521c0148b35037016f641083e45573
ForkadulIslam <forkadulislam@gmail.com>
2023-11-26 17:46:46 +0600

    port update

--- index.ts
+++ index.ts
@@ -37,4 +37,4 @@ router.post('/api/postTest', (req, res) => {
 
 app.use(router)
 
-app.listen(3001, () => { console.log('Example app listening on port 3001!') })
+app.listen(3000, () => { console.log('Example app listening on port 3000!') })
