6a7629e676687fe019f5b747b2a1081f4f963593
ForkadulIslam <forkadulislam@gmail.com>
2023-11-27 19:58:59 +0600

    Dbconfig update

--- Models/dbconfig.js
+++ Models/dbconfig.js
@@ -2,10 +2,16 @@ const { Sequelize } = require("sequelize");
 
 const sequelize = new Sequelize({
   dialect: "mysql",
-  host: "localhost",
-  port: 8889,
-  username: "root",
-  password: "root",
-  database: "ui_commercial",
+  // host: "localhost",
+  // port: 8889,
+  // username: "root",
+  // password: "root",
+  // database: "ui_commercial",
+  
+  host: "127.0.0.1",
+  port: 3306,
+  username: "uicommercial",
+  password: "Uicom@123",
+  database: "uicommercial_db",
 });
 module.exports = sequelize;
